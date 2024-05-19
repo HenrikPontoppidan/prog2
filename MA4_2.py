47b78f6e1e2abca4f944846f1f8e4acae8aefc84
HenrikPontoppidan <120667862+HenrikPontoppidan@users.noreply.github.com>
2024-05-19 23:57:58 +0200

    Update MA4_2.py

--- MA4_2.py
+++ MA4_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
 
 def fib_py(n):
 	if n <= 1:
@@ -8,16 +9,19 @@ def fib_py(n):
 	else:
 		return(fib_py(n-1) + fib_py(n-2))
 
+@njit
 def fib_numba(n):
-	a, b = 0, 1
-	for _ in range(n-1):
-		a, b = b, a + b
-	return b
-
+	if n <= 1:
+		return n
+	else:
+		return(fib_numba(n-1) + fib_numba(n-2))
 
 def main():
-	f = Person(50)
+	f = Person(30)
 	print(f.fib())
+
+	print(fib_py(30))
+	print(fib_numba(30))
 
 if __name__ == '__main__':
 	main()
